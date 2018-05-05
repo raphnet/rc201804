de08510b8ee033198f2d9add37021224e4b4d69e
Raphael Assenat <raph@raphnet.net>
2018-05-05 10:40:31 -0400

    Adjust message position based on screen dimensions

--- messagescreen.asm
+++ messagescreen.asm
@@ -1,5 +1,11 @@
 section .text
 
+%define TEXTFONT_HEIGHT	8
+%define MSGBOX_HEIGHT	32
+%define MSGBOX_ORG_Y	(SCREEN_HEIGHT/2-MSGBOX_HEIGHT/2)
+%define MSGBOX_BORDER_THICKNESS	4
+%define MSGBOX_TEXT_Y	(SCREEN_HEIGHT/2-TEXTFONT_HEIGHT/2)
+
 	;;;;; messageScreen_start
 	;
 	; Prepare for a message screen by saving the screen content and
@@ -13,21 +19,24 @@ messageScreen_start:
 	push dx
 
 	call savescreen
-	mov cx, effect_height(200)
+	mov cx, effect_height(SCREEN_HEIGHT)
 	call eff_checkboard
 
-	mov bx,40
-	mov cx, 32
+	; Message background (clear)
+	mov bx, MSGBOX_ORG_Y
+	mov cx, MSGBOX_HEIGHT
 	mov ax, 0
 	call clearLinesRange
 
-	mov bx, 40
-	mov cx, 4
+	; Top border
+	mov bx, MSGBOX_ORG_Y
+	mov cx, MSGBOX_BORDER_THICKNESS
 	mov al, 3
 	call clearLinesRange
 
-	mov bx, 56
-	mov cx, 4
+	; Bottom border
+	mov bx, MSGBOX_ORG_Y + MSGBOX_HEIGHT - MSGBOX_BORDER_THICKNESS
+	mov cx, MSGBOX_BORDER_THICKNESS
 	mov al, 3
 	call clearLinesRange
 
@@ -47,10 +56,10 @@ messageScreen_end:
 	push bx
 	push cx
 
-	mov bx,40
-	mov cx, 32
-	mov ax, 0
-	call clearLinesRange
+;	mov bx,40
+;	mov cx, 32
+;	mov ax, 0
+;	call clearLinesRange
 
 	call restorescreen
 
@@ -67,8 +76,8 @@ messageScreen_end:
 	;
 	; Returns with AX and BX set for centered string
 messageScreen_drawText_prepare:
-	mov ax, 160
+	mov ax, SCREEN_WIDTH/2
 	call subHalfStrwidthFromAX
-	mov bx, 46
+	mov bx, MSGBOX_TEXT_Y
 	ret
 
